87383273d284fdfbe0dc43f869af66ab8af2bc45
PolMacDonnacha <47214404+PolMacDonnacha@users.noreply.github.com>
2020-10-28 19:02:29 +0000

    Aded chromeoption to run without displaying chrome. Split strings to remove time from launch site etc. Added if statements to check for inconsistent format on site

--- Program.cs
+++ Program.cs
@@ -3,60 +3,77 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Linq;
 using System.Collections.Generic;
-
 using static System.Console;
-
-
     class Program
     {
         static void Main(string[] args)
         {
-            int myindex = 0;
+            ChromeOptions options = new ChromeOptions(); 
+		options.AddArguments("headless"); 
+            int myindex = 0; // counts total number of records found on the website
             //List<IWebElement> IWdatenames = new List<IWebElement>();
-            string[] urls = {"http://spaceflightnow.com/launch-log-2004-2008/", "http://spaceflightnow.com/launch-log-2009-2011/", "http://spaceflightnow.com/launch-log-2012-2014/", "https://spaceflightnow.com/launch-log-2015-2016/","https://www.Spaceflightnow.com/launch-schedule/"};
+            string[] urls = {
+                "http://spaceflightnow.com/launch-log-2004-2008/",
+                 "http://spaceflightnow.com/launch-log-2009-2011/",
+                  "http://spaceflightnow.com/launch-log-2012-2014/",
+                   "https://spaceflightnow.com/launch-log-2015-2016/",
+                   "https://spaceflightnow.com/launch-log/",
+                   "https://www.Spaceflightnow.com/launch-schedule/"};
             foreach(string url in urls)
             {
-
-            
-            IWebDriver driver = new ChromeDriver(@"D:\000College Work\Year3\Semester1\WebProgramming2\TestingScrapers\bin\Debug\netcoreapp3.1\");
+            IWebDriver driver = new ChromeDriver(@"D:\000College Work\Year3\Semester1\WebProgramming2\TestingScrapers\bin\Debug\netcoreapp3.1\",options);
             driver.Url = url;
 
-            WriteLine("This is the title of the viewed page" + driver.Title);
-            IReadOnlyCollection<IWebElement> IWdatename = driver.FindElements(By.ClassName("datename"));//includes date, vehicle and mission name
-            foreach(var datename in IWdatename.Select((value, index) => new {value, index})){
-                WriteLine("This is IWDateName {0}: {1}",myindex,datename.value.Text.ToString());
-                myindex++;
+            WriteLine("Page Title: " + driver.Title);
+            IReadOnlyCollection<IWebElement> IWdatename = driver.FindElements(By.ClassName("datename"));//div holding mission records
+
+
+             List<IWebElement> dateNameList = new List<IWebElement>();
+             if (url == "https://www.Spaceflightnow.com/launch-schedule/")
+            {
+                dateNameList = IWdatename.ToList();
+            }
+            else
+            {
+                dateNameList = IWdatename.Reverse().ToList(); //inverts the order of the objects since previous pages have latest missions first
+            }
+
+            IWebElement IWlaunchdate = driver.FindElement(By.ClassName("launchdate"));
+            IWebElement IWVehicleTitle = driver.FindElement(By.ClassName("mission"));//includes vehicle and name
+            foreach(var datename in dateNameList.Select(value => new {value}))
+            {           
+            IWebElement IWmissionData = datename.value.FindElement(By.XPath("following-sibling::*[1]"));//launchtime and site
+            IWebElement IWDescription = datename.value.FindElement(By.XPath("following-sibling::*[2]"));//Mission Description
+             string VehicleAndTitle = IWVehicleTitle.Text.ToString();//convert to string for separation         
+                String[] missionSplt = VehicleAndTitle.Split('•',StringSplitOptions.RemoveEmptyEntries);
+                string vehicle = missionSplt[0];
+                string missionName = missionSplt[1];
+                WriteLine("{0} Mission Name: {1}",myindex, missionName);
+                WriteLine("Launch Vehicle: " + vehicle);
+
+            string timeAndSite = IWmissionData.Text.ToString();
+            string launchDate = IWlaunchdate.Text.ToString();
+            if(timeAndSite.Contains("\n")) //edge case on page 2012-2014 where launchsite doesn't go to a new line, in else statement find closing bracket after time to separate
+            {
+                String[] timeSiteSplt = timeAndSite.Split("\n" ,StringSplitOptions.None); //separates the launch time from the launch site
+                string time = timeSiteSplt[0].Remove(timeSiteSplt[0].IndexOf("L"),12).Trim(); //removes the string "Launch Time: " from the time and site data and also any spaces at the ends of the data
+                string launchSite = timeSiteSplt[1].Remove(timeSiteSplt[1].IndexOf("L"),12).Trim();
+                WriteLine("Date and Time: {0} {1}",launchDate,time);
+                WriteLine("Launch Site: " + launchSite);
+            }
+            else
+            {
+                int separator = timeAndSite.IndexOf(")") + 1;// finds the space after the closing bracket of the time property
+                String[] timeSiteSplt = timeAndSite.Split(timeAndSite[separator] ,StringSplitOptions.None); //separates every string by spaces
+                string time = string.Format("{0} {1} {2}{3} {4}",timeSiteSplt[2], timeSiteSplt[3], timeSiteSplt[4], timeSiteSplt[5], timeSiteSplt[6]); //concatenates the strings needed
+                string launchSite = string.Format("{0} {1} {2} {3} {4} {5} {6}",timeSiteSplt[9], timeSiteSplt[10], timeSiteSplt[11], timeSiteSplt[12], timeSiteSplt[13], timeSiteSplt[14], timeSiteSplt[15]);
+                WriteLine("Date and Time: {0} {1}",launchDate,time);
+                WriteLine("Launch Site: " + launchSite);
+            }
+            myindex++;
             }
             
-            IWebElement IWlaunchdate = driver.FindElement(By.ClassName("launchdate"));//date of launch
-            string launchdate = IWlaunchdate.Text.ToString();
-            WriteLine("This is the date of the most recent mission on the site" + launchdate);
-            IWebElement IWmission = driver.FindElement(By.ClassName("mission"));//includes vehicle and name
-            string VehicleAndTitle = IWmission.Text.ToString();//convert to string for separation
-            String[] missionSplt = VehicleAndTitle.Split('•',StringSplitOptions.RemoveEmptyEntries);
-            string vehicle = missionSplt[0];
-            WriteLine("Launch Vehicle: " + vehicle);
-            string missionName = missionSplt[1];
-            WriteLine("Mission Name: " + missionName);
-            IWebElement IWmissionData = driver.FindElement(By.ClassName("missiondata"));//launchtime and site
-            string timeAndSite = IWmissionData.Text.ToString();
-            String[] timeSiteSplt = timeAndSite.Split("\n" ,StringSplitOptions.None); //separates the launch time from the launch site
-            foreach(var item in timeSiteSplt)
-            {
-                WriteLine("Time or Launch Site: " + item);
-            }
-            string time = timeSiteSplt[0];
-            WriteLine("Time of launch: " + time);
-            string launchSite = timeSiteSplt[1];
-            WriteLine("Launch Site: " + launchSite);            
-           
-
-        
             driver.Quit();
             }
         }
     }
-
-/* foreach(var driver.FindElement(By.ClassName("datename")) in driver.Url){         foreach date on the page, create new object with data
-
-            }*/
